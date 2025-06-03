cb0032d0b8a9057c18355e56ab980e1b4080d4fd
Tiwari <saurabh.i.tiwari@accenture.com>
2025-06-03 12:29:48 +0530

    Updated existing files and added the few more methods in LL

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -18,7 +18,7 @@ public class LinkedList {
         Node head = new Node(arr[0]);
         Node mover = head;
 
-        for(int i=0; i<arr.length; i++) {
+        for(int i=1; i<arr.length; i++) {
             Node temp = new Node(arr[i]);
             mover.next = temp;
             mover = temp;
@@ -26,9 +26,40 @@ public class LinkedList {
         return head;
     }
 
+    public static int findLengthOfLL(Node head) {
+        Node temp = head;
+        int count = 0;
+
+        while(temp!=null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    public static void traverseLL(Node head) {
+        Node temp = head;
+        while(temp!=null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
+    public static int checkIfPresent(Node head, int val) {
+        Node temp = head;
+        while(temp != null) {
+            if(temp.data == val) return 1;
+            temp = temp.next;
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
         int[] arr = {2,5,6,7};
-        Node y = new Node(arr[3]);
-        System.out.println(y.data);
+        Node head = convertArr2LL(arr);
+
+        System.out.println(checkIfPresent(head, 54));
+        
+
     }
 }
